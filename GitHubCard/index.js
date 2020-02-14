65d978f5aeabbdf80f66e930db9256366947dee5
Tiffany Crosby <tiffany.crosby65@gmail.com>
2020-02-13 20:24:26 -0500

    finished followers cards. was able to make all info appear that was available.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,45 +2,33 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-axios.get('https://api.github.com/users/TiffanyCrosby')
-  .then(response => {
-    console.log(response);
-    // response.data.forEach(item => {
-    //   let newPerson = people(item);
-    cards.appendChild(people(response.data));
-  })
-  .catch(error => {
-    console.log('Data Not Returned', error);
-  });
 
-
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+/* Step 2: Inspect and study the data coming back, this is YOUR
+   github info! You will need to understand the structure of this
+   data in order to use it to build your component function
 
    Skip to Step 3.
 */
 
-/* Step 4: Pass the data received from Github into your function, 
+/* Step 4: Pass the data received from Github into your function,
            create a new component and add it to the DOM as a child of .cards
 */
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
+/* Step 5: Now that you have your own card getting added to the DOM, either
+          follow this link in your browser https://api.github.com/users/<Your github name>/followers
+          , manually find some other users' github handles, or use the list found
           at the bottom of the page. Get at least 5 different Github usernames and add them as
           Individual strings to the friendsArray below.
-          
+
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
+*/
 
-<div class="card">
+{/* <div class="card">
   <img src={image url of user} />
   <div class="card-info">
     <h3 class="name">{users name}</h3>
@@ -49,17 +37,41 @@ const followersArray = [];
     <p>Profile:
       <a href={address to users github page}>{address to users github page}</a>
     </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
+  <p>Followers: {users followers count}</p>
+  <p>Following: {users following count}</p>
+  <p>Bio: {users bio}</p>
 </div>
+</div > */}
 
-*/
+const followersArray = ["teaguehannam", "dakoriah", "idongessien", "b-dubz79", "David-E-Alvarez"];
+
+
+axios.get('https://api.github.com/users/TiffanyCrosby')
+  .then(response => {
+    console.log(response);
+    // response.data.forEach(item => {
+    //   let newPerson = people(item);
+    cards.prepend(people(response.data));
+  })
+  .catch(error => {
+    console.log('Data Not Returned', error);
+  });
+
+followersArray.forEach((follower) => {
+  axios.get(`https://api.github.com/users/${follower}`)
+    .then(response => {
+      const newCard = people(response.data);
+      cards.appendChild(newCard);
+    });
+})
+  .catch(error => {
+    console.log('Data Not Returned', error);
+  });
+
 
 let cards = document.querySelector('.cards');
 
-function people(data) {
+let people = (data) => {
 
   //create new elements
   let card = document.createElement('div');
@@ -85,8 +97,9 @@ function people(data) {
   name.textContent = data.name;
   userName.textContent = data.login;
   location.textContent = `Location: ${data.location}`;
-  profile.textContent = `Profile: ${data.url}`;
+  profile.textContent = `Profile: `;
   pgUrl.href = data.url;
+  pgUrl.textContent = data.url;
   followers.textContent = `Followers: ${data.followers}`;
   following.textContent = `Following: ${data.following}`;
   bio.textContent = `Bio: ${data.bio}`;
@@ -98,7 +111,6 @@ function people(data) {
   cardInfo.append(userName);
   cardInfo.append(location);
   cardInfo.append(profile);
-  profile.append(pgUrl);
   cardInfo.append(followers);
   cardInfo.append(following);
   cardInfo.append(bio);
